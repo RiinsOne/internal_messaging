c27801140ac102b09b6b57743d5177f35b735d68
RiinsOne <riins@bk.ru>
2020-02-05 22:32:03 +0300

    add local_settings

---
+++ myapp/local_settings_example.py
@@ -0,0 +1,31 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2+ytr(-%krxo@*s9e)^$t%8r_k=88rk3y@u@4!9$56gxvp7z#z'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- myapp/old/settings_05FEB20.py
+++ myapp/old/settings_05FEB20.py
@@ -177,12 +177,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-try:
-    from local_settings import *
-except ImportError:
-    print('Warning! local_settings are not defined!')
